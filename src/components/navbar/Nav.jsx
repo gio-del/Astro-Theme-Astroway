a5e29f7c2290ce65de7e136a7360c70a79e3315f
gio-del <giovanni.delucia@polimi.it>
2024-03-24 12:53:06 +0100

    Navbar backdrop on mobile

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -4,7 +4,7 @@ import Search from "../search/Search";
 import Hamburger from "./Hamburger";
 import { info } from "../../data/info";
 
-export default function Nav({ posts, absolute }) {
+export default function Nav({ posts }) {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const navLinks = [
@@ -23,96 +23,81 @@ export default function Nav({ posts, absolute }) {
     return initials;
   };
 
-  useEffect(() => {
-    const handleClick = (e) => {
-      if (isNavOpen) {
-        if (!e.target.closest("nav")) {
-          setIsNavOpen(false);
-        }
-      }
-    };
-
-    document.addEventListener("click", handleClick);
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-  }, [isNavOpen]);
-
   return (
-    <nav
-      className={
-        "w-full top-0 inset-x-0 dark:bg-dk-primary bg-primary z-50" +
-        (absolute ? " absolute" : "")
-      }
-    >
-      <div className="w-full px-6 py-2 flex justify-between items-center">
-        <a className="font-bold text-2xl lg:text-4xl" href="/#">
-          <span className="text-secondary dark:text-dk-secondary">
-            {"</" + extractInitials(info.name) + ">"}
-          </span>
-        </a>
+    <>
+      <nav className="w-full top-0 inset-x-0 dark:bg-dk-primary bg-primary z-50 absolute">
+        <div className="w-full px-6 py-2 flex justify-between items-center">
+          <a className="font-bold text-2xl lg:text-4xl" href="/#">
+            <span className="text-secondary dark:text-dk-secondary">
+              {"</" + extractInitials(info.name) + ">"}
+            </span>
+          </a>
 
-        {/* Button for CV download */}
-        <a
-          href={info.cv}
-          download
-          className="inline-flex items-center px-4 py-2 border-2 rounded text-secondary dark:text-dk-secondary border-secondary dark:border-dk-secondary appearance-none focus:outline-none hover:bg-secondary dark:hover:bg-dk-secondary hover:text-primary dark:hover:text-primary transition duration-300 ease-in-out cursor-pointer"
-        >
-          <i className="fas fa-download mr-2"></i>
-          <span className="hidden lg:inline-block">Download CV</span>
-          <span className="lg:hidden">CV</span>
-        </a>
+          {/* Button for CV download */}
+          <a
+            href={info.cv}
+            download
+            className="px-4 py-2 border-2 rounded text-secondary dark:text-dk-secondary border-secondary dark:border-dk-secondary hover:bg-secondary dark:hover:bg-dk-secondary hover:text-primary dark:hover:text-primary transition duration-300 ease-in-out cursor-pointer absolute left-1/2 transform -translate-x-1/2"
+          >
+            <i className="fas fa-download mr-2"></i>
+            <span className="hidden lg:inline-block">Download CV</span>
+            <span className="lg:hidden">CV</span>
+          </a>
 
-        <div className="inline-flex lg:hidden text-secondary dark:text-dk-secondary">
-          <Hamburger
-            onClick={() => setIsNavOpen(!isNavOpen)}
-            isNavOpen={isNavOpen}
-          />
+          <div className="inline-flex lg:hidden text-secondary dark:text-dk-secondary">
+            <Hamburger
+              onClick={() => setIsNavOpen(!isNavOpen)}
+              isNavOpen={isNavOpen}
+            />
+          </div>
+          <div className="hidden lg:block">
+            <ul className="inline-flex text-secondary dark:text-dk-secondary text-2xl font-normal">
+              {navLinks.map((link, index) => (
+                <li
+                  key={index}
+                  className="p-4 hover:text-accent dark:hover:text-dk-accent"
+                >
+                  <a href={link.href}>{link.name}</a>
+                </li>
+              ))}
+              <li className="px-4 flex">
+                <ToggleDarkMode />
+              </li>
+              <li className="px-4 flex">
+                <Search posts={posts} />
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="hidden lg:block">
-          <ul className="inline-flex text-secondary dark:text-dk-secondary text-2xl font-normal">
+        <div
+          className={
+            !isNavOpen
+              ? "hidden"
+              : "" +
+                " h-full flex flex-col items-center text-center lg:hidden dark:text-tertiary"
+          }
+        >
+          <ul className="w-full text-secondary dark:text-dk-secondary text-xl font-semibold">
             {navLinks.map((link, index) => (
-              <li
-                key={index}
-                className="p-4 hover:text-accent dark:hover:text-dk-accent"
-              >
-                <a href={link.href}>{link.name}</a>
+              <li key={index} className="p-4">
+                <a href={link.href} onClick={() => setIsNavOpen(false)}>
+                  {link.name}
+                </a>
               </li>
             ))}
-            <li className="px-4 flex">
+            <li className="p-4 flex flex-row items-center justify-evenly">
               <ToggleDarkMode />
-            </li>
-            <li className="px-4 flex">
               <Search posts={posts} />
             </li>
           </ul>
         </div>
-      </div>
-      <div
-        className={
-          !isNavOpen
-            ? "hidden"
-            : "" +
-              " h-full flex flex-col items-center text-center lg:hidden dark:text-tertiary"
-        }
-      >
-        <ul
-          className="w-full text-secondary dark:text-dk-secondary text-xl font-semibold"
-          onClick={() => setIsNavOpen(!isNavOpen)}
-        >
-          {navLinks.map((link, index) => (
-            <li key={index} className="p-4">
-              <a href={link.href}>{link.name}</a>
-            </li>
-          ))}
-          <li className="p-4">
-            <ToggleDarkMode />
-          </li>
-          <li className="p-4">
-            <Search posts={posts} />
-          </li>
-        </ul>
-      </div>
-    </nav>
+      </nav>
+      {isNavOpen && (
+        <div
+          className="fixed inset-0 blur-3xl bg-black bg-opacity-50 z-40"
+          onClick={() => setIsNavOpen(false)}
+        ></div>
+      )}
+    </>
   );
 }
