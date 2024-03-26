c2f13fbf28b122a1e95916cc3de351bba930de05
gio-del <giovanni.delucia@polimi.it>
2024-03-26 12:55:33 +0100

    accessible emoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [rehypeAccessibleEmojis],
+  },
   site: 'https://astro-portfolio-template.pages.dev',
   integrations: [tailwind(), react(), robotsTxt(), sitemap()]
 });
